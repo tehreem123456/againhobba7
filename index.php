12809b60c7cf49386a83ef292dc643a32253036c
tehreem123456 <137266904+tehreem123456@users.noreply.github.com>
2023-11-30 14:36:08 +0500

    add

---
+++ index.php
@@ -0,0 +1,721 @@
+<?php
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!-- HTML TAGS IN HEADER,  EDIT EVERYTHING, INCLUSIVE REL CANONICAL -->
+
+<title>강남호스트바 예약 필수! 주말 놓치지 마세요!</title>
+<link rel="canonical" href="/index.php" />
+<link rel="shortcut icon" href="img/againhobba-logo.png" />
+<meta charset="UTF-8">
+<meta name="description" content="강남호스트바 예약 필수! 주말을 더 특별하게 만들어보세요. 편안한 좌석을 놓치지 마세요. 지금 예약하세요!">
+<meta name="keywords" content=""강남 호스트바 예약, 주말 놓치지 마세요, 특별한 주말 예약, 강남 밤문화 예약, 호스트바 좌석 확보"">
+<meta name="author" content="againhobba">
+
+
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css">
+
+  <!-- STYLESHEET, REMEMBER TO EDIT BACKGROUND IMAGE URLS -->
+
+  <style>
+    @import url(\'https://fonts.googleapis.com/css2?family=Domine:wght@400;700&family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap\');
+
+    * {
+      font-family: \'Open Sans\', sans-serif;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    body {
+      font-family: \'Montserrat\', sans-serif;
+    }
+
+    a {
+      text-decoration: none;
+    }
+
+    .top-sec a {
+      text-shadow: 1px 1px #000000;
+    }
+
+    .boxes ul {
+      float: unset !important;
+      list-style: disc;
+      margin-left: 30px;
+    }
+
+    .boxes ul li {
+      margin-bottom: 5px;
+    }
+    .boxes ul li a {
+      color: #000000;
+    }
+
+
+    .container {
+      padding: 10px 70px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+
+    .logo-box a {
+      outline: none;
+      display: block;
+    }
+
+    .logo-box img {
+      display: block;
+      max-width: 225px;
+    }
+
+    nav {
+      overflow: hidden;
+    }
+
+    ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      float: right;
+    }
+
+    nav li {
+      display: inline-block;
+      margin-left: 25px;
+      height: 70px;
+      line-height: 70px;
+      transition: .5s linear;
+    }
+
+    nav a {
+      text-decoration: none;
+      display: block;
+      position: relative;
+      color: #868686;
+      text-transform: uppercase;
+    }
+
+    nav a:after {
+      content: "";
+      width: 0;
+      height: 2px;
+      position: absolute;
+      left: 0;
+      bottom: 15px;
+      background: #868686;
+      transition: width .5s linear;
+    }
+
+    nav a:hover:after {
+      width: 100%;
+    }
+
+
+    .sec-k {
+      text-align: center;
+    }
+
+    @media screen and (max-width: 660px) {
+      header {
+        text-align: center;
+      }
+
+      .logo-box {
+        float: none;
+        display: inline-block;
+        margin: 0 0 16px 0;
+      }
+
+      ul {
+        float: none;
+      }
+
+      nav li:first-of-type {
+        margin-left: 0;
+      }
+    }
+
+    @media screen and (max-width: 550px) {
+      nav {
+        overflow: visible;
+      }
+
+      nav li {
+        display: block;
+        margin: 0;
+        height: 40px;
+        line-height: 40px;
+      }
+
+      nav li:hover {
+        background: rgba(0, 0, 0, .1);
+      }
+
+      nav a:after {
+        content: none;
+      }
+    }
+
+
+
+    .top-sec {
+      background-image: linear-gradient(to bottom, #1010109b, #1010109b), url(img/main.jpg);
+      background-position: center center;
+      background-size: cover;
+      background-repeat: no-repeat;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 130px 70px;
+    }
+
+    .top-sec p,
+    .top-sec h1,
+    .top-sec a {
+      text-align: center;
+      color: #fff;
+    }
+    .top-sec h1 {
+      font-size: 50px;
+      margin-bottom: 30px;
+    }
+    .top-sec p {
+      margin-bottom: 10px;
+    }
+
+    .wrapper-content {
+      max-width: 1300px;
+      margin: 0 auto;
+    }
+
+    .center {
+      text-align: center;
+    }
+
+    .icon-wrap {
+      display: flex;
+    }
+
+    .icon-box {
+      text-align: center;
+      width: 50%;
+      margin-top: 3rem;
+    }
+
+    .price {
+      color: #ff7e0c;
+      font-size: 42px;
+    }
+
+    .cta-btn {
+      letter-spacing: 1.5px;
+      fill: #fff;
+      color: #fff;
+      background-color: #ff7e0c;
+      border-radius: 36px 36px 36px 36px;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, .5);
+      padding: 10px 20px 10px 20px;
+      position: relative;
+      top: 1rem;
+      text-decoration: none;
+    }
+
+    .image-wrap {
+      margin-top: 0;
+      padding: 20px 70px;
+      background: #F7F8FA;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, .5) inset;
+      transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
+    }
+
+    .image-wrap2 {
+      margin-top: 5rem;
+      margin-bottom: 5rem;
+      padding-bottom: 5rem;
+      background: #F7F8FA;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, .5) inset;
+      transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
+    }
+
+    .boxes {
+      display: flex;
+      padding-top: 2rem;
+      padding-bottom: 0rem;
+    }
+
+
+    .box {
+      width: 50%;
+    }
+    .box h2 {
+      margin-bottom: 10px;
+    }
+
+    .box:nth-of-type(1) {
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: 50% 50%;
+    }
+
+    .imagebox {
+      max-width: 100%;
+      width: 90%;
+      padding-left: 35px;
+      padding-top: 50px;
+      box-shadow: 1px 1px 11px -1px #ccc;
+      border-radius: 5px;
+      margin: 20px 0;
+    }
+
+    .imagebox:nth-of-type(1) {
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: 50% 50%;
+    }
+
+    .text {
+      text-align: left;
+      padding: 0px 25px;
+      word-break: break-word;
+    }
+
+
+    .grid-container {
+      display: grid;
+      grid-template-columns: auto auto;
+      padding: 10px;
+    }
+
+
+    .spacing {
+      padding-bottom: 3rem;
+      padding-left: 3rem;
+    }
+
+    footer .row {
+      display: flex;
+      align-items: flex-start;
+      justify-content: flex-start;
+    }
+    .column {
+      /* float: left; */
+      width: 25%;
+      padding: 2rem 0rem;
+    }
+    footer .column p {
+      margin-bottom: 5px;
+    }
+
+    /* Clear floats after the columns */
+    .row:after {
+      content: "";
+      display: table;
+      clear: both;
+    }
+
+    footer {
+      background: #3A3A3A;
+      color: #fff;
+      font-size: 14px;
+      padding: 20px 70px;
+    }
+
+    footer a {
+      color: #fff;
+      text-decoration: none;
+      display: block;
+      margin-bottom: 5px;
+    }
+
+    .sub {
+      font-size: 18px;
+      font-weight: 600;
+      margin-bottom: 15px !important;
+    }
+
+    /* Wrapper */
+    .icon-button {
+      background-color: white;
+      border-radius: 2.6rem;
+      cursor: pointer;
+      display: inline-block;
+      font-size: 1.3rem;
+      height: 2.6rem;
+      line-height: 2.6rem;
+      margin: 0 5px;
+      position: relative;
+      text-align: center;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      width: 2.6rem;
+    }
+
+    /* Circle */
+    .icon-button span {
+      border-radius: 0;
+      display: block;
+      height: 0;
+      left: 50%;
+      margin: 0;
+      position: absolute;
+      top: 50%;
+      -webkit-transition: all 0.3s;
+      -moz-transition: all 0.3s;
+      -o-transition: all 0.3s;
+      transition: all 0.3s;
+      width: 0;
+    }
+
+    .icon-button:hover span {
+      width: 2.6rem;
+      height: 2.6rem;
+      border-radius: 2.6rem;
+      margin: -1.3rem;
+    }
+
+    /* Icons */
+    .icon-button i {
+      background: none;
+      color: white;
+      height: 2.6rem;
+      left: 0;
+      line-height: 2.6rem;
+      position: absolute;
+      top: 0;
+      -webkit-transition: all 0.3s;
+      -moz-transition: all 0.3s;
+      -o-transition: all 0.3s;
+      transition: all 0.3s;
+      width: 2.6rem;
+      z-index: 10;
+    }
+
+
+    .twitter span {
+      background-color: #4099ff;
+    }
+
+    .facebook span {
+      background-color: #3B5998;
+    }
+
+    .google-plus span {
+      background-color: #789cc1;
+    }
+
+    .tumblr span {
+      background-color: #34526f;
+    }
+
+    .instagram span {
+      background-color: #517fa4;
+    }
+
+    .youtube span {
+      background-color: #bb0000;
+    }
+
+    .pinterest span {
+      background-color: #0A66C2;
+    }
+
+    .icon-button .icon-twitter {
+      color: #4099ff;
+    }
+
+    .icon-button .icon-facebook {
+      color: #3B5998;
+    }
+    .icon-button .fa-pinterest {
+      color: #db5a3c;
+    }
+
+    .icon-button .fa-tumblr {
+      color: #34526f;
+    }
+
+    .icon-button .icon-google-plus {
+      color: #db5a3c;
+    }
+
+    .icon-button .fa-instagram {
+      color: #517fa4;
+    }
+
+    .icon-button .fa-youtube {
+      color: #bb0000;
+    }
+
+    .icon-button .fa-linkedin {
+      color: #0A66C2;
+    }
+
+
+
+
+    .icon-button:hover .icon-twitter,
+    .icon-button:hover .icon-facebook,
+    .icon-button:hover .icon-google-plus,
+    .icon-button:hover .fa-tumblr,
+    .icon-button:hover .fa-instagram,
+    .icon-button:hover .fa-youtube,
+    .icon-button:hover .fa-linkedin,
+    .icon-button:hover .fa-pinterest {
+      color: white;
+    }
+
+    @media all and (max-width: 680px) {
+      .icon-button {
+        border-radius: 1.6rem;
+        font-size: 0.8rem;
+        height: 1.6rem;
+        line-height: 1.6rem;
+        width: 1.6rem;
+      }
+
+      .icon-button:hover span {
+        width: 1.6rem;
+        height: 1.6rem;
+        border-radius: 1.6rem;
+        margin: -0.8rem;
+      }
+
+      /* Icons */
+      .icon-button i {
+        height: 1.6rem;
+        line-height: 1.6rem;
+        width: 1.6rem;
+      }
+
+      .pinterest {
+        display: none;
+      }
+
+    }
+
+
+
+    @media all and (max-width: 1130px) {
+
+      .top-sec,
+      .icon-wrap,
+      .boxes {
+        display: block;
+      }
+
+      iframe {
+        width: 95%;
+      }
+
+      .text {
+        padding: 0px;
+      }
+
+      .box,
+      .icon-box {
+        width: 100%;
+        text-align: center;
+        display: block;
+      }
+
+      .wrapper-content {
+        padding: 50px;
+      }
+
+      .top-sec {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 50px;
+        height: 350px;
+        font-size: 26px !important;
+      }
+
+      .spacing {
+        padding-bottom: 3rem;
+        padding-top: 1rem;
+        padding-left: 0rem;
+      }
+
+      .icon-box {
+        margin-bottom: 7rem;
+      }
+
+      .top-sec a {
+        font-size: 24px !important;
+      }
+
+      .column {
+        width: 50%;
+      }
+
+      .link a {
+        font-size: 2.4rem;
+      }
+    }
+  </style> 
+
+</head>
+    
+<body>
+<header>
+
+
+ <div class="container">
+  
+  <!-- INSERT LOGO URL AND /how-to-use-twitter-moments-for-local-seo.html -->
+  
+    <div class="logo-box">
+      <a href="/index.php">
+        <img alt="againhobba-logo" src="img/againhobba-logo.png" id="againhobba-logo" data-test-source="againhobba-logo" data-test-pro-id="againhobba-logo" data-test-pro-name="againhobba-logo" data-test="againhobba-logo" title="againhobba-logo">
+      </a>
+    </div>
+    <nav>
+    <ul>
+    
+    <!-- NAVIGATION MENU INSERT ALL GOOGLE PROPERTIES AND UMBRELLA PROPERTIES -->
+    
+      <li><a href="https://againhobba.com/">강남호스트바</a></li>
+   </ul>
+  </nav>
+  </div>
+</header>
+
+
+<section class="top-sec">
+
+<div>
+
+<!-- INSERT MAIN KEYWORDS IN THE H1 TAG, AND INSERT NAP INFO BELOW -->
+
+<h1><span id="강남호스트바 예약 필수! 주말 놓치지 마세요!"><a href="https://againhobba.com/">강남호스트바 예약 필수! 주말 놓치지 마세요!</a></span></h1>
+<p><a href="https://againhobba.com/" target="_none">강남호스트바 예약 필수! 주말 놓치지 마세요!</a></p>
+</div>
+    
+</section>
+
+<!-- INSERT CONTENT AND LINKS TO MONEYSITE -->
+
+
+<section class="image-wrap">
+    <div class="wrapper-content boxes">
+    <div class="box">     
+	
+    
+    </div>
+    <div class="box spacing">
+    
+<!-- INSERT MAIN CONTENT, AND LINKS -->
+    <strong>주말에 강남호스트바 예약 필수?</strong>
+<h2>Introduction</h2>
+<strong>강남 호스트바 소개</strong>
+강남의 호스트바 컨셉 소개 및 주말에 호스트바를 예약하는 것이 필요한지에 대한 아이디어 던지기.
+<h2>Understanding 호스트바 Culture in Gangnam</h2>
+<strong>강남 호스트바 문화 이해</strong>
+강남 호스트바의 독특한 측면 공개 및 활기찬 야간 문화에서 예약의 역할.
+<h2>The Allure of Gangnam 호스트바</h2>
+<strong>강남 호스트바의 매력</strong>
+강남 호스트바가 인기 있는 이유에 대한 토론 및 분위기, 엔터테인먼트 및 사회적 매력.
+<h2>Weekend Rush and Limited Availability</h2>
+<strong>주말 혼잡과 제한된 가용성</strong>
+강남 호스트바에서 주말 혼잡 상황 강조 및 예약 없이 좌석을 확보하는 과제.
+<h2>Advantages of Weekend Reservations</h2>
+<strong>주말 예약의 장점</strong>
+호스트바를 예약하는 이점 탐색 및 보장된 좌석, 맞춤 서비스 등.
+<h2>Choosing the Right 호스트바 for the Weekend</h2>
+<strong>주말 경험을 위한 적절한 호스트바 선택</strong>
+주말 경험을 위한 이상적인 장소 선택에 대한 팁과 강남 호스트바 간의 차별화 요소.
+<h2>Making a Reservation: Easy Steps</h2>
+<strong>예약하기: 간단한 단계</strong>
+예약하는 간단한 단계 안내 및 독자에게 번거로움 없는 프로세스 제공.
+<h2>Customer Experiences with Reservations</h2>
+<strong>예약 고객의 경험</strong>
+예약한 고객들의 긍정적인 이야기 공유 및 예약이 어떻게 경험을 향상시켰는지.
+<h2>Weekend 호스트바 Etiquette</h2>
+<strong>주말 호스트바 예절</strong>
+주말 호스트바 방문에 대한 올바른 예절 논의 및 독자가 경험에 잘 대비할 수 있도록.
+<h2>FAQs About Weekend 호스트바 Reservations</h2>
+<strong>주말 호스트바 예약에 관한 FAQs</strong>
+예약에 관한 일반적인 질문에 대한 답변 제공 및 독자에게 명확하고 가치 있는 정보 제공.
+<h2>Conclusion</h2>
+<strong>결론</strong>
+강남 호스트바를 예약하는 이점 요약 및 독자들에게 잊지 못할 주말을 위한 사전 계획을 권장.
+  
+    </div>
+    </div>
+</section >
+
+    
+
+    
+<!-- INSERT GOOGLE MAPS EMBED, IF COMPANY HAS GMB PROFILE, INCLUDE CID IN THE URL -->
+
+
+ 
+
+
+<!-- INSERT SECONDARY KEYWORD ANCHOR TEXT LINKS TO GOOGLE MAPS INCLUDING CID IN THE URL. IF NO GOOGLE MAPS PROFILE EXISTS, THEN LINK TO GOOGLE NEW SITE -->
+    
+
+
+
+
+
+
+    
+<footer>
+    <div class="wrapper-content">
+        <div class="row">
+  <div class="column"><img src="img/againhobba-logo.png" alt="againhobba-logo" style="max-width: 225px;">
+            
+     <!-- INSERT COMPANY INFO -->
+Location<br>
+강남구 삼성동 143-35
+
+Our hours<br>
+24/7/365
+
+Contact us<br>
+Phone : 010 2999 2467
+            
+       </div>
+       
+       <!-- INSERT LINKS TO SERVICES IN MAIN MONEY SITE -->
+       
+<div class="column"><p class="sub">In progress</p>
+
+
+
+</div>
+
+<!-- INSERT LINKS TO BLOG POSTS -->
+<div>
+<H2>In progress</H2>
+<p>
+
+
+</p>
+    </div>
+</div>
+    </div>
+    
+    
+    </footer>
+
+
+<!-- Simple cloud HTML template created by Jesper Nissen SEO at https://jespernissen.com -->
+
+</body>
+
+</html>
+';
+?>
